2f118afc855e2ed97a6b9f4c737c7e4d3e4d8b09
Audrius Dobrovolskis <audrius@scorchsoft.com>
2015-07-29 12:47:53 +0100

    editing plugin

--- assets/plugins/pdf/pdf.js
+++ assets/plugins/pdf/pdf.js
@@ -23,7 +23,7 @@ if (!RedactorPlugins) var RedactorPlugins = {};
                 // make your added button as Font Awesome's icon
                 this.button.setAwesome('advanced', 'fa-tasks');
 
-                console.log(this.opts.figuresList);
+                //console.log(this.opts.figuresList);
 
 
             },
@@ -36,14 +36,18 @@ if (!RedactorPlugins) var RedactorPlugins = {};
 
                 this.modal.createCancelButton();
 
-                var button = this.modal.createActionButton('Insert');
-                button.on('click', this.pdf.insert);
+                //var button = this.modal.createActionButton('Insert');
+                //button.on('click', this.pdf.insert);
 
                 this.selection.save();
                 this.modal.show();
 
+                var root = this;
+
+
                 $.getJSON(this.opts.figuresList, $.proxy(function(data)
                 {
+
 
                     $.each(data, $.proxy(function(key, val)
                     {
@@ -51,50 +55,42 @@ if (!RedactorPlugins) var RedactorPlugins = {};
                         //console.log(key);
                         // title
                         var thumbtitle = '';
+                        var item_id = '';
                         if (typeof val.title !== 'undefined') thumbtitle = val.title;
+                        if (typeof val.figure_id !== 'undefined') item_id = val.figure_id;
 
-
-                        var img = $('<div class="redactor-figure-list-item row"><div class="image col-md-4"><img src="' + val.thumb + '" class="img-responsive" title="' + thumbtitle + '" />' + '</div><div class="title col-md-8"><a href="javascript:void(0)" title="' + thumbtitle + '"> <p>' + thumbtitle + '</p></a></div></div>');
+                        var img = $('<div class="redactor-figure-list-item row"><div class="image col-md-4"><img src="' + val.thumb + '" class="img-responsive" title="' + thumbtitle + '" />' + '</div><div class="title col-md-8"><p>' + thumbtitle + '</p></div><div class="button-row col-md-12"> <a href="javascript:void(0)" class="btn btn-default button-figure" data-id="'+ item_id +'">Insert Figure</a> <a href="javascript:void(0)" class="btn btn-default button-modal" data-id="'+ item_id +'">Insert Modal</a></div></div>');
 
 
                         $('#redactor_figures_box').append(img);
 
-                        $(img).click(function(){
-
-                            var text = $('#selection-marker-2').map(function(){
-                                return this.previousSibling.nodeValue;
-                            });
-                            $("#selection-marker-2").after(" <a href='/"+val.title+"' target='_blank'>"+text[0]+"</a> ");
-                            var text = $('#selection-marker-2').map(function(){
-                                // console.log(this.previousSibling);
-                                // this.previousSibling.remove();
-                                this.previousSibling.parentNode.removeChild(this.previousSibling);
-
-                            });
-
-                            $('.redactor-selection-marker').remove();
-
-                            var content_val = $( ".redactor_editor" ).html();
-                            $("textarea#CmsPage_body").val(content_val);
-                            $("textarea#CmsBlog_content").val(content_val);
-                            $("textarea#Events_event_txt").val(content_val);
-                            $("textarea#EmailTemplate_body").val(content_val);
-                            $("textarea#Remembrances_body").val(content_val);
-                            $("textarea#Homepage_body").val(content_val);
-
-
-                        });
-
-                        $(img).click($.proxy(this.imageCallbackLink, this));
-
-
 
                     }, this));
 
+                    $('.button-modal').click(function(){
+                        var clicked_id = $(this).data('id');
+                        console.log(clicked_id);
+
+                        var input_string = '[[m-'+clicked_id+']]';
+
+                        root.modal.close();
+                        root.insert.html(input_string);
+
+                    });
+
+                    $('.button-figure').click(function(){
+                        var clicked_id = $(this).data('id');
+                        console.log(clicked_id);
+
+                        var input_string = '[[f-'+clicked_id+']]';
+
+                        root.modal.close();
+                        root.insert.html(input_string);
+
+                    });
+
 
                 }, this));
-
-
 
                 $('#mymodal-textarea').focus();
             },
